3a7c4b97be37f72248c92911ce020730914d745d
inimaz <93inigo93@gmail.com>
2025-08-13 16:33:50 +0200

    feat: team section

---
+++ src/assets/contributors/contributors.js
@@ -0,0 +1,51 @@
+/*
+Adding a new contributor : 
+You are in front of an array, you can add a contributor by following the scheme.
+Copy/paste the example, and replace with your own information. 
+
+    {
+        name: "Your_data_here",
+        job : "Your_data_here",
+        Organisation: "Your_data_here",
+        img_path: "./assets/contributors/img/victor.jpeg"
+    },
+*/
+
+export const data = [
+  {
+    name: "Benoît Courty",
+    job: "Data Scientist",
+    Organisation: "",
+    img_path: "/assets/contributors/img/Benoit_Courty.jpg",
+  },
+  {
+    name: "Amine Saboni",
+    job: "Deep Learning Engineer",
+    Organisation: "",
+    img_path: "/assets/contributors/img/Amine_Saboni.jpg",
+  },
+  {
+    name: "Sasha Luccioni",
+    job: "Researcher",
+    Organisation: "Hugging Face",
+    img_path: "/assets/contributors/img/sascha.jpg",
+  },
+  {
+    name: "Iñigo Imaz",
+    job: "Software Developer",
+    Organisation: "",
+    img_path: "/assets/contributors/img/inigo.jpg",
+  },
+  {
+    name: "Luis Blanche",
+    job: "Machine Learning Engineer",
+    Organisation: "",
+    img_path: "/assets/contributors/img/luis.jpg",
+  },
+  {
+    name: "Patrick",
+    job: "Software developer",
+    Organisation: "",
+    img_path: "/assets/contributors/img/patrick.jpg",
+  },
+];
