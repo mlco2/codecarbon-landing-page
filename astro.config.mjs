09abf8f4542e6f3d088fa726f674d5cecb5528bb
inimaz <93inigo93@gmail.com>
2025-08-21 17:56:09 +0200

    deploy: setup github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mlco2.github.io",
+  base: "/codecarbon-landing-page/",
   vite: {
     plugins: [tailwindcss()],
   },
--- src/content/partners.ts
+++ src/content/partners.ts
@@ -22,32 +22,32 @@ Copy/paste the example, and replace with your own information.
 export const founders: Founder[] = [
   {
     name: "BCG Gamma",
-    svg_path: "/assets/partners/bcg_gamma.svg",
+    svg_path: "assets/partners/bcg_gamma.svg",
   },
   {
     name: "Comet",
-    svg_path: "/assets/partners/comet.svg",
+    svg_path: "assets/partners/comet.svg",
   },
   {
     name: "Haverford College",
-    svg_path: "/assets/partners/haverford_college.svg",
+    svg_path: "assets/partners/haverford_college.svg",
   },
   {
     name: "Mila",
-    svg_path: "/assets/partners/mila.svg",
+    svg_path: "assets/partners/mila.svg",
   },
 ];
 export const sponsors: Sponsor[] = [
   {
     name: "Clever Cloud",
-    svg_path: "/assets/partners/clever_cloud.png",
+    svg_path: "assets/partners/clever_cloud.png",
   },
   {
     name: "GitHub",
-    svg_path: "/assets/partners/GitHub.png",
+    svg_path: "assets/partners/GitHub.png",
   },
   {
     name: "Mozilla",
-    svg_path: "/assets/partners/mozilla.svg",
+    svg_path: "assets/partners/mozilla.svg",
   },
 ];
