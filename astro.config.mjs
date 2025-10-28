c0bb0f9a382a54e1aaf70260129ebc97837fd401
inimaz <93inigo93@gmail.com>
2025-10-28 13:47:04 +0100

    feat: publish to codecarbon.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mlco2.github.io",
-  base: "/codecarbon-landing-page/",
+  site: "https://codecarbon.io",
   vite: {
     plugins: [tailwindcss()],
   },
