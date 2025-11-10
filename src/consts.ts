beece48b3bc35cb7b02554afaf811131399e4fcd
inimaz <49730431+inimaz@users.noreply.github.com>
2025-11-10 14:17:10 +0100

    Update src/consts.ts
    Co-authored-by: Benoît Courty <6603048+benoit-cty@users.noreply.github.com>

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = "CodeCarbon";
-export const SITE_DESCRIPTION = "Welcome to CodeCarbon!";
+export const SITE_DESCRIPTION = "Track & reduce Co2 emissions from your computing";
 
 export const DASHBOARD_API_URL = "https://dashboard.codecarbon.io/";
 export const GITHUB_CODECARBON_URL = "https://github.com/mlco2/codecarbon";
